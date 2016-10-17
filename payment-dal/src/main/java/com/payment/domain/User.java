7709c5e22d208f1749691b1e7e9225d2de5baca6
zhoujiapei <zhoujiapei@qianmi.com>
2016-10-17 14:24:09 +0800

    用户信息注释完善

--- payment-dal/src/main/java/com/payment/domain/User.java
+++ payment-dal/src/main/java/com/payment/domain/User.java
@@ -7,39 +7,89 @@ import java.util.Date;
 
 public class User  extends AbstractEntity implements Serializable {
 
-
+    /**
+     * 绑定用户编号
+     */
     private String userId;
 
+    /**
+     * 支付密码
+     */
     private String payPsw;
 
+    /**
+     * 是否使用支付密码
+     */
     private Short isUsePayPsw;
 
+    /**
+     * 输入无效密码次数
+     */
     private Short countInvalidPwd;
 
+    /**
+     * 密码强度
+     */
     private Short pwdStrength;
 
+    /**
+     * 交易密码的base key 密钥
+     */
     private String payBaseKey;
 
+    /**
+     * 支付密码的 密码类型 0老密码 1 新密码
+     */
     private Short payPswType;
 
+    /**
+     * 证件类型
+     */
     private Short idType;
 
+    /**
+     * 证件号
+     */
     private String idNo;
 
-    private Short userState;
+    /**
+     * 绑定用户编号
+     */
+    private Short acctState;
 
+    /**
+     * 真实姓名
+     */
     private String realName;
 
+    /**
+     * 电子邮箱
+     */
     private String emaill;
 
+    /**
+     * 手机号码
+     */
     private String mobile;
 
+    /**
+     * 实名认证
+     */
     private Short realCheck;
 
+    /**
+     * 注册IP
+     */
     private String regIp;
 
+    /**
+     * 注册时间
+     */
     private Date regTime;
 
+    /**
+     * 店铺名称
+     */
     private String companyName;
 
     private static final long serialVersionUID = 1L;
@@ -116,12 +166,12 @@ public class User  extends AbstractEntity implements Serializable {
         this.idNo = idNo == null ? null : idNo.trim();
     }
 
-    public Short getUserState() {
-        return userState;
+    public Short getAcctState() {
+        return acctState;
     }
 
-    public void setUserState(Short userState) {
-        this.userState = userState;
+    public void setAcctState(Short acctState) {
+        this.acctState = acctState;
     }
 
     public String getRealName() {
@@ -195,7 +245,7 @@ public class User  extends AbstractEntity implements Serializable {
         sb.append(", payPswType=").append(payPswType);
         sb.append(", idType=").append(idType);
         sb.append(", idNo=").append(idNo);
-        sb.append(", userState=").append(userState);
+        sb.append(", acctState=").append(acctState);
         sb.append(", realName=").append(realName);
         sb.append(", emaill=").append(emaill);
         sb.append(", mobile=").append(mobile);
